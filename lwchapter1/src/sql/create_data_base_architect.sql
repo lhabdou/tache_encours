2fcef6cf8f1a25091b4846b97e3a3280ad4d5559
asoilihi <lhabdou26@hotmail.fr>
2018-08-20 17:19:23 +0200

    aso - ajout de deux tables tasks et taskCategories

--- lwchapter1/src/sql/create_data_base_architect.sql
+++ lwchapter1/src/sql/create_data_base_architect.sql
@@ -47,4 +47,76 @@ CREATE UNIQUE INDEX users_fkindex1
   (roleid);
 
 
+-- création des taches 
+
+
+  CREATE TABLE public.TaskCategories(
+  
+  taskCategoryID integer NOT NULL,
+  taskCategory character varying(20),
+
+  CONSTRAINT TaskCategories_pkey PRIMARY KEY (taskCategoryID)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.TaskCategories
+  OWNER TO postgres;
+
+-- Index: public.users_fkindex1
+
+-- DROP INDEX public.users_fkindex1;
+CREATE UNIQUE INDEX TaskCategories_fkindex1
+  ON public.TaskCategories
+  USING btree
+  (taskCategoryID);
+
+
+  CREATE TABLE public.Tasks
+(
+  taskID integer NOT NULL,
+  taskCategoryID integer NOT NULL,
+  userID integer NOT NULL,
+  description varchar(500),
+  completed boolean,
+  dateCreated date,
+  dateCompleted date, 
+  dateScheduled date,
+  
+  
+  CONSTRAINT Tasks_pkey PRIMARY KEY (taskID),
+  CONSTRAINT fk_Tasks_users FOREIGN KEY (userID)
+      REFERENCES public.users (userID) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT fk_Tasks_taskCategories FOREIGN KEY (taskCategoryID)
+      REFERENCES public.taskCategories(taskCategoryID) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.Tasks
+  OWNER TO postgres;
+
+-- Index: public.users_fkindex1
+
+-- DROP INDEX public.users_fkindex1;
+CREATE UNIQUE INDEX tasks_fkindex1
+  ON public.Tasks
+  USING btree
+  (taskID);
+  
+CREATE UNIQUE INDEX tasks_fkindex2
+  ON public.Tasks
+  USING btree
+  (userID);
+
+CREATE UNIQUE INDEX tasks_fkindex3
+  ON public.Tasks
+  USING btree
+  (taskCategoryID);
+
+
+
+
 
